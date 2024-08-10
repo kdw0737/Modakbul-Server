34b3ee986e9410f175833f86a32717fae2d3c78e
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-08-10 16:44:43 +0900

    [FEAT] 카페 정렬, 조회 기능 구현
    - 카페를 리스트로 보여줄 때 거리순으로 정렬하는 기능을 구현했습니다.
    - 카페를 리스트로 보여줄 때 모임순, 거리순으로 정렬하는 기능을 구현했습니다.
    - 카페를 이름으로 검색하여 리스트로 보여줄 때 거리순으로 조회하는 기능을 구현했습니다.

---
+++ modakbul/src/main/java/com/modakbul/domain/cafe/controller/CafeController.java
@@ -0,0 +1,45 @@
+package com.modakbul.domain.cafe.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.modakbul.domain.cafe.dto.CafeResDto;
+import com.modakbul.domain.cafe.service.CafeService;
+import com.modakbul.global.common.response.BaseResponse;
+import com.modakbul.global.common.response.BaseResponseStatus;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class CafeController {
+
+	private final CafeService cafeService;
+
+	@GetMapping("/cafes/distance")
+	public BaseResponse<List<CafeResDto.CafeListDto>> getCafeListSortByDistance(
+		@RequestParam(value = "latitude") double latitude, @RequestParam(value = "longitude") double longitude) {
+		return new BaseResponse<>(BaseResponseStatus.SEARCH_SORT_BY_DISTANCE_SUCCESS,
+			cafeService.getCafeListSortByDistance(latitude, longitude));
+	}
+
+	@GetMapping("/cafes/meeting")
+	public BaseResponse<List<CafeResDto.CafeListDto>> getCafeListSortByMeetingCount(
+		@RequestParam(value = "latitude") double latitude, @RequestParam(value = "longitude") double longitude) {
+		return new BaseResponse<>(BaseResponseStatus.SEARCH_SORT_BY_MEETING_COUNT_SUCCESS,
+			cafeService.getCafeListSortByMeetingCount(latitude, longitude));
+	}
+
+	@GetMapping("/cafes")
+	public BaseResponse<List<CafeResDto.CafeListDto>> searchCafeList(
+		@RequestParam(value = "name") String cafeName,
+		@RequestParam(value = "latitude") double latitude,
+		@RequestParam(value = "longitude") double longitude
+	) {
+		return new BaseResponse<>(BaseResponseStatus.SEARCH_CAFE_SUCCESS,
+			cafeService.searchCafeList(cafeName, latitude, longitude));
+	}
+}
