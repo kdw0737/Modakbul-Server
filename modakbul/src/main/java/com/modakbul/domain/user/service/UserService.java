55a58925195db6a56335f303918f9582c5f72364
김도욱 <kdw981129@gmail.com>
2024-08-21 16:54:33 +0900

    [FEAT] 신고하기 변수 수정 및 불필요한 코드 제거

--- modakbul/src/main/java/com/modakbul/domain/user/service/UserService.java
+++ modakbul/src/main/java/com/modakbul/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.modakbul.domain.user.service;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,20 +8,22 @@ import java.util.stream.Stream;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.modakbul.domain.block.entity.Block;
+import com.modakbul.domain.block.reposiroty.BlockRepository;
 import com.modakbul.domain.board.dto.BoardInfoDto;
 import com.modakbul.domain.board.entity.Board;
 import com.modakbul.domain.board.repository.BoardRepository;
 import com.modakbul.domain.match.entity.Matches;
 import com.modakbul.domain.match.enums.MatchStatus;
 import com.modakbul.domain.match.repository.MatchRepository;
-import com.modakbul.domain.report.repository.Blockrepository;
 import com.modakbul.domain.user.dto.BlockListResDto;
 import com.modakbul.domain.user.dto.MeetingsHistoryResDto;
+import com.modakbul.domain.user.dto.MyProfileReqDto;
+import com.modakbul.domain.user.dto.MyProfileResDto;
+import com.modakbul.domain.user.dto.UserCafeResDto;
 import com.modakbul.domain.user.dto.UserProfileResDto;
-import com.modakbul.domain.user.dto.UserRequestDto;
-import com.modakbul.domain.user.dto.UserResponseDto;
 import com.modakbul.domain.user.entity.Category;
 import com.modakbul.domain.user.entity.User;
 import com.modakbul.domain.user.entity.UserCategory;
@@ -31,6 +34,7 @@ import com.modakbul.domain.user.repository.UserCategoryRepository;
 import com.modakbul.domain.user.repository.UserRepository;
 import com.modakbul.global.common.response.BaseException;
 import com.modakbul.global.common.response.BaseResponseStatus;
+import com.modakbul.global.s3.service.S3ImageService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -41,18 +45,19 @@ public class UserService {
 	private final UserCategoryRepository userCategoryRepository;
 	private final CategoryRepository categoryRepository;
 	private final UserRepository userRepository;
-	private final BoardRepository boardRepository;
+	private final S3ImageService s3ImageService;
 	private final MatchRepository matchRepository;
-	private final Blockrepository blockrepository;
+	private final BlockRepository blockRepository;
+	private final BoardRepository boardRepository;
 
-	public UserResponseDto.ProfileDto findProfile(User user) {
-		List<UserCategory> findUserCategories = userCategoryRepository.findCategoryByUser(user);
+	public MyProfileResDto getMyProfileDetails(User user) {
+		List<UserCategory> findUserCategories = userCategoryRepository.findAllByUserIdWithCategory(user.getId());
 
 		List<CategoryName> findCategories = findUserCategories.stream()
 			.map(userCategory -> userCategory.getCategory().getCategoryName())
 			.collect(Collectors.toList());
 
-		return UserResponseDto.ProfileDto.builder()
+		return MyProfileResDto.builder()
 			.image(user.getImage())
 			.nickname(user.getNickname())
 			.isVisible(user.getIsVisible())
@@ -62,7 +67,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public void modifyProfile(User user, UserRequestDto.ProfileDto request) {
+	public void updateMyProfile(User user, MultipartFile image, MyProfileReqDto request) {
 		userCategoryRepository.deleteAllByUser(user);
 
 		List<UserCategory> userCategories = request.getCategories().stream()
@@ -76,8 +81,24 @@ public class UserService {
 			}).collect(Collectors.toList());
 		userCategoryRepository.saveAll(userCategories);
 
-		user.update(request);
+		s3ImageService.deleteImageFromS3(user.getImage());
+
+		user.update(s3ImageService.upload(image), request);
 		userRepository.save(user);
+	}
+
+	public List<UserCafeResDto> getCafesHistory(User user) {
+		List<Matches> findMatches = matchRepository.findAllByParticipantIdWithCafe(user.getId(),
+			MatchStatus.ACCEPTED, LocalDate.now());
+
+		return findMatches.stream()
+			.map(findMatch -> UserCafeResDto.builder()
+				.cafeId(findMatch.getBoard().getCafe().getId())
+				.name(findMatch.getBoard().getCafe().getName())
+				.image(findMatch.getBoard().getCafe().getImageUrls().get(0))
+				.streetAddress(findMatch.getBoard().getCafe().getAddress().getStreetAddress())
+				.build())
+			.collect(Collectors.toList());
 	}
 
 	@Transactional(readOnly = true)
@@ -185,7 +206,7 @@ public class UserService {
 
 	@Transactional(readOnly = true)
 	public List<BlockListResDto> getBlockedUserList(User user) { //TODO : 쿼리 최적화 고려
-		List<Block> findBlockList = blockrepository.findAllByBlockerId(user.getId());
+		List<Block> findBlockList = blockRepository.findAllByBlockerId(user.getId());
 		List<Long> blockedIds = findBlockList.stream()
 			.map(findBlock -> findBlock.getBlockedId().getId())
 			.collect(Collectors.toList());
