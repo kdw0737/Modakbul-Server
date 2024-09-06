ef7acf715318bd7ea23705fda65bd61d51dc17bf
Dowook Kim <kdw981129@gmail.com>
2024-09-06 23:15:52 +0900

    [CHORE] 충돌 중복 코드 삭제

--- modakbul/src/main/java/com/modakbul/domain/cafe/enums/Congestion.java
+++ modakbul/src/main/java/com/modakbul/domain/cafe/enums/Congestion.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.modakbul.domain.cafe.enums;
 
 public enum Congestion {
 	CROWDED, NORMAL, QUIET
 }
-=======
-package com.modakbul.domain.cafe.enums;
-
-public enum Congestion {
-	CROWDED, NORMAL, QUIET
-}
->>>>>>> e2b2879279d196a48e04d5a4824fdce288a5471a
--- modakbul/src/main/java/com/modakbul/domain/cafe/enums/OpeningStatus.java
+++ modakbul/src/main/java/com/modakbul/domain/cafe/enums/OpeningStatus.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.modakbul.domain.cafe.enums;
 
 public enum OpeningStatus {
 	OPEN, CLOSED
 }
-=======
-package com.modakbul.domain.cafe.enums;
-
-public enum OpeningStatus {
-	OPEN, CLOSED
-}
->>>>>>> e2b2879279d196a48e04d5a4824fdce288a5471a
